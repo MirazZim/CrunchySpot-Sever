e8e450bd0c0d0897282aa13c8000ad9016f54cf3
MirazZim <miraz.zim.38@gmail.com>
2025-02-28 00:22:45 +0600

    Did the delete function in the dashboard / cart work. now when i delete it refetch and it is dynamically

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 //MongoDB
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlurp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,6 +47,15 @@ async function run() {
         res.send(result);
     })
 
+    app.delete("/carts/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+    })
+
+
+    //Menu Api  (CRUD)        
 
     app.get("/menu", async (req, res) => {
         const result = await menuCollection.find().toArray();
