13de80d6261a21d5ee4d18bfcb5c11be82a3c3ae
MirazZim <miraz.zim.38@gmail.com>
2025-03-02 14:49:18 +0600

    earlier a user who is not admin can get access if they change the url to http://localhost:5173/dashboard/users they can easily get the admin panel but after functionality of verifyingAdmin is done they cant do it and whenever they do the function of verifyAdmin directly make them log out

--- index.js
+++ index.js
@@ -57,10 +57,22 @@ async function run() {
       })
     }
 
+    //use verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ admin: false , message: 'Forbidden access!'});
+      }
+      next();
+    }
+
 
     //users related apis
 
-  app.get('/users',verifyToken, async (req, res) => {
+  app.get('/users',verifyToken, verifyAdmin, async (req, res) => {
     
     const result = await userCollection.find().toArray();
     res.send(result);
